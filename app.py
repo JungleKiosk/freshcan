07e49ac86bd33296c64cb501d135da14b4387b39
Fra <fra24eso@gmail.com>
2024-08-18 23:56:14 +0200

    🍜test jinja: {{data.test}}

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+import data
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home/home.html')
+    return render_template('home/home.html', data=data)
+
 
